97c718004c1dea8091f83f89989e15dbeaba0448
Stisat <119959404+Stisat@users.noreply.github.com>
2024-04-21 13:01:21 +0300

    Update1 task.py
    last update

--- HW/task.py
+++ HW/task.py
@@ -3,8 +3,7 @@ import subprocess
 
 def sub(command: str, text: str):
     res = subprocess.run(str(command), shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-    out = res.stdout.split('\n')
-    if str(text) in out and not res.returncode:
+    if str(text) in res.stdout and not res.returncode:
         return True
     else:
         return False
